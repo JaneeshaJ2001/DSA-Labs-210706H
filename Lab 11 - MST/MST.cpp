138f3a6390ec9e2a33eb926b53bb288ce7da2c72
JaneeshaJ2001 <wickramasinghejj.21@uom.lk>
2023-06-06 14:06:54 +0530

    changed

--- Lab 11 - MST/MST.cpp
+++ Lab 11 - MST/MST.cpp
@@ -4,9 +4,7 @@ using namespace std;
 // Number of vertices in the graph
 #define V 6
 
-// A utility function to find the vertex with
-// minimum key value, from the set of vertices
-// not yet included in MST
+// minKey function finds the vertex with minimum key value, from the set of vertices not yet included in MST
 int minKey(int key[], bool mstSet[])
 {
 	// Initialize min value
@@ -19,20 +17,20 @@ int minKey(int key[], bool mstSet[])
 	return min_index;
 }
 
-// A utility function to print the
-// constructed MST stored in parent[]
-void printMST(int parent[], int graph[V][V])
+// print the constructed MST stored in parent[]
+void printMST(int parent[], int graph[V][V], int start)
 {
 	cout << "Edge \tWeight\n";
-	for (int i = 1; i < V; i++)
-		cout << parent[i] << " - " << i << " \t"
-			<< graph[i][parent[i]] << " \n";
+	for (int i = 0; i < V; i++){
+        if (i!= start){
+            cout << parent[i] << " - " << i << " \t" << graph[i][parent[i]] << " \n";
+        }
+    }
+		
 }
 
-// Function to construct and print MST for
-// a graph represented using adjacency
-// matrix representation
-void primMST(int graph[V][V])
+// construct and print MST for a graph represented using adjacency matrix representation
+void primMST(int graph[V][V], int start)
 {
 	// Array to store constructed MST
 	int parent[V];
@@ -47,46 +45,40 @@ void primMST(int graph[V][V])
 	for (int i = 0; i < V; i++)
 		key[i] = INT_MAX, mstSet[i] = false;
 
-	// Always include first 1st vertex in MST.
-	// Make key 0 so that this vertex is picked as first
-	// vertex.
-	key[0] = 0;
+	// let's start with node 0 ,make key 0 
+	key[start] = 0;
 
 	// First node is always root of MST
-	parent[0] = -1;
+	parent[start] = -1;
 
 	// The MST will have V vertices
 	for (int count = 0; count < V - 1; count++) {
 		
-		// Pick the minimum key vertex from the
-		// set of vertices not yet included in MST
+		// Pick the minimum key vertex from the set of vertices not yet included in MST
 		int u = minKey(key, mstSet);
 
 		// Add the picked vertex to the MST Set
 		mstSet[u] = true;
 
-		// Update key value and parent index of
-		// the adjacent vertices of the picked vertex.
-		// Consider only those vertices which are not
-		// yet included in MST
+		// Update key value and parent index of the adjacent vertices of the picked vertex.
+		// Consider only those vertices which are not yet included in MST
 		for (int v = 0; v < V; v++)
 
-			// graph[u][v] is non zero only for adjacent
-			// vertices of m mstSet[v] is false for vertices
-			// not yet included in MST Update the key only
-			// if graph[u][v] is smaller than key[v]
+			// Update the key only if graph[u][v] is smaller than key[v]
 			if (graph[u][v] && mstSet[v] == false
 				&& graph[u][v] < key[v])
 				parent[v] = u, key[v] = graph[u][v];
 	}
 
 	// Print the constructed MST
-	printMST(parent, graph);
+	printMST(parent, graph, start);
 }
 
-// Driver's code
+
 int main()
 {
+    int start = 1;
+
 	int graph[V][V] = { { 0, 3, 0, 0, 0, 1 },
 						{ 3, 0, 2, 1, 10, 0 },
 						{ 0, 2, 0, 3, 0, 5 },
@@ -95,9 +87,7 @@ int main()
                         { 1, 0, 5, 0, 4, 0} };
 
 	// Print the solution
-	primMST(graph);
+	primMST(graph, start);
 
 	return 0;
 }
-
-// This code is contributed by rathbhupendra
